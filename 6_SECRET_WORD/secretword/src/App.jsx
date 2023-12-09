c78d93fb0e55078a00421ea1df3663c49dafeb2c
EderJrDev <ederjuninho2003@gmail.com>
2023-12-09 11:52:47 -0300

    feat: create new project secret word

---
+++ 6_SECRET_WORD/secretword/src/App.jsx
@@ -0,0 +1,78 @@
+//CSS
+import "./App.css";
+//React
+import { useState } from "react";
+// data
+import { wordsList } from "./data/words";
+//components
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+import StartScreen from "./components/StartScreen";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
+function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategoty] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    // pick a rando caregory
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    //pick a random word
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    return { word, category };
+  };
+
+  // starts the secret word game
+  const startGame = () => {
+    // pick word and pick category
+    const { word, category } = pickWordAndCategory();
+
+    // create an array of letters
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(word, category);
+    console.log(wordLetters);
+
+    // fill states
+    setPickedWord(word);
+    setPickedCategoty(category);
+    setLetters(wordLetters);
+
+    setGameStage(stages[1].name);
+  };
+  //process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+  //process the letter input
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
+  return (
+    <>
+      <div className="App">
+        {gameStage === "start" && <StartScreen startGame={startGame} />}
+        {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+        {gameStage === "end" && <GameOver retry={retry} />}
+      </div>
+    </>
+  );
+}
+
+export default App;
